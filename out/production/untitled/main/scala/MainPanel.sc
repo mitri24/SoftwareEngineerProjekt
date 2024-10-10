65cd4696854e1e02de25d07e05785d5a39728c99
Miréio Trinley <mireio.trinley@uni-konstanz.de>
2024-10-10 22:18:47 +0200

    Grundstrukturen

---
+++ out/production/untitled/main/scala/MainPanel.sc
@@ -0,0 +1,17 @@
+// das Spielbrett auf dem sich die Figur final bewegt
+
+object playground {
+
+  println("|"*4)
+
+
+}
+
+// zum schreiben von Code, um die Figur auf dme Spielfeld zu bewegen.
+
+
+object IDEA {}
+
+
+//scala.io.StdIn.readLine()
+
--- untitled/src/main/scala/HelloWorld.scala
+++ untitled/src/main/scala/HelloWorld.scala
@@ -1,51 +1,152 @@
+package main.scala
+
+import scala.io.StdIn.readLine
+
 object HelloWorld {
   def main(args: Array[String]): Unit = { // hier werden argumente mitgegeben
-    val name = "Mark"
-    val age = 18 // val bedeutet value
-    var res = " " //var bedeutet variable
-    var y = 0
-    println("Hello, World")
 
-    //STRING INTERPOLATION : wir wollen einen gegebenen String mit einem neuen Wert ersetzen
+    println("Rooobert, Zeit zu coooden!")
+    println(IDEA.instruction())
+    //println(IDEA.inputLine())
 
-    println(name + " is " + age + " years old"); // eine möglichkeit der Verbindung
-    println(s"$name is $age years old"); // s string interpolation,
-    // ist aber nicht Typ-sicher
-    println(f"$name%s is $age%d years old"); //f string interpolation
-    // $s bedeutet, dass wir eine Stringvariable meinen
-    // %d steht für integer
-    // %f steht für float
-    println(raw"hello \nWorld"); // raw string interpolation
-    // \n wird auch geprintet,
 
-    //IF ELSE STATEMENTS:
-    if (18 == age) { //identisch wie in Java
-      println("true")
-      res = "age == 20"
+
+  }
+
+  def checkInput(): Unit = {
+    //    val input = scala.io.StdIn.readLine()
+    //    val solution = true
+    //    // Durchlaufe jedes Zeichen im Input-String
+    //    for (zeichen <- input) {
+    //      // Vergleiche jedes Zeichen mit der Lösung
+    //      if (zeichen == solution) {
+    //        println("true")
+    //        return true // Gebe sofort true zurück, wenn die Lösung gefunden wurde
+    //      } else {
+    //        println("false")
+    //      }
+
+  }
+
+
+  object Movement {
+
+    //Methode zur Bewegung der Figur auf dem Spielfeld
+    def moveRight(): Unit = {
+      println("Right")
+    }
+
+    //Methode zur Bewegung der Figur auf dem Spielfeld
+    def moveLeft(): Unit = {
+      println("Left ... Left")
+    }
+
+    //Methode zur Bewegung der Figur auf dem Spielfeld
+    def moveForward(): Unit = {
+      println("straight")
+    }
+
+    //Methode zur Drehung der Figur auf dem Spielfeld
+    def turn(): Unit = {}
+
+
+  }
+
+
+  //Figur-implementierung
+  object figure {
+
+    val seize = 1.0 // LückenfüllerGröße
+
+    printf("x")
+
+  }
+
+  //Diamanten-implementierung
+  object Jerm {}
+
+
+  object playground {
+
+
+    def builtPlayground(): Unit = {
+
+      printf("  _ " * 4 + "\n")
+
+
+      for (counter <- 0 until 2) {
+        printf("| _ " * 4)
+        printf("|  " + "\n")
+
+      }
+
 
     }
-    else {
-      println("false")
-      res = "age != 20"; //reassignment
+
+
+  }
+
+  object IDEA {
+
+    def movement (input: List[String]): Unit = { // können es rekursiv machen
+      input.foreach {
+        case "moveForward()" => Movement.moveForward()
+        case "moveRight()" => Movement.moveRight()
+        case "moveLeft()" => Movement.moveLeft()
+        case _ => printf("Hilf Robert")
+        // Anweisungen, wenn keiner der oben genannten Fälle zutrifft
+      }
     }
 
-    println(res)
-    val res2 = if (age == 20) "age ==20" else "age !=20" // das ist eine andere Syntax für if else
-    println(res2)
-    // while (age <30){}
+    def inputLine(): Unit = {
 
-    do {
-      y += 1
-    } while (y < 10)
+      var input = readLine()
+      var inputList: List[String] = List()
 
+      while (input != "start") {
+        input = readLine()
+        inputList = inputList :+ input
+      }
+
+      IDEA.movement(inputList)
+
+
+      // ein Wert, den du vergleichen möchtest
+
+
+
+
+    }
+
+    def instruction(): Unit = {
+      println("Coding - Anleitung")
+
+      println("Robert will den Jerm. Hilf ihm dabei, indem du die Methoden " +
+        "moveForward(), moveRight(), moveLeft() und collectJerm() kombinierst.\n" +
+        "Wähle das Level mit '1' oder '2' und drücke ENTER")
+
+      inputLine()
+
+//      printf("  _ " * 10 + "\n")
+//
+//
+//      for (counter <- 0 until 5) {
+//        println("|  " + "   " * 12 + "  |")
+//      }
+//      printf("  _ " * 10 + "\n")
+    }
 
   }
 }
 
-//Object is a class, aber es hat bereits eine Instanz. Also können wir keinen neues Keyword an diesem Object benutzen
-// Hello World ist der Name des Objektes
-//def: methode wird deklariert
-//jedes Scala project muss eine Main-methode haben
+
+
+
+
+
+
+
+
 
 
 
